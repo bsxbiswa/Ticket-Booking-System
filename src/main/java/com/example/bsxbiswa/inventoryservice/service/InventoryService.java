f2c48941c2b4a2919f7f42e24550e7cad53a2f21
bsxbiswa <biswajitbiswalbs7@gmail.com>
2025-10-22 23:04:44 +0530

    created repository services and entitry classes for Event and Venue

--- src/main/java/com/example/bsxbiswa/inventoryservice/service/InventoryService.java
+++ src/main/java/com/example/bsxbiswa/inventoryservice/service/InventoryService.java
@@ -1,13 +1,26 @@
 package com.example.bsxbiswa.inventoryservice.service;
 
+import com.example.bsxbiswa.inventoryservice.repository.EventRepository;
+import com.example.bsxbiswa.inventoryservice.repository.VenueRepository;
 import com.example.bsxbiswa.inventoryservice.response.EventInventoryResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class InventoryService {
-    public List<EventInventoryResponse> getAllEvents (){
+
+    private final EventRepository eventRepository;
+    private final VenueRepository venueRepository;
+
+    @Autowired
+    public InventoryService( final EventRepository eventRepository, final VenueRepository venueRepository){
+        this.eventRepository = eventRepository;
+        this.venueRepository = venueRepository;
+    }
+
+    public List<EventInventoryResponse> getAllEvents () {
 
     }
 }
